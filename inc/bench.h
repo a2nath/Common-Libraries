97ac37a64a5d2804df27629d38a2a76dbedbd7fa
Abhimanyu Nath <32167617+a2nath@users.noreply.github.com>
2025-01-18 12:52:17 -0500

    Add FPS Bench and use template for original
    Added FPSBench for benchmarking fps that needs floating point value. 
    
    FPS = 1.0f/frametime
    Use template and make milliseconds the default type for precision in the original BenchMark code

--- inc/bench.h
+++ inc/bench.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <chrono>
 
-struct Bench
+template<class Precision = std::chrono::milliseconds>
+struct BenchMark
 {
 	std::chrono::steady_clock::time_point begin;
 	std::chrono::steady_clock::time_point end;
@@ -21,17 +22,52 @@ struct Bench
 
 	inline void mark()
 	{
-		delta = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - mark_time).count();
+		delta = std::chrono::duration_cast<Precision>(std::chrono::steady_clock::now() - mark_time).count();
 		mark_time = std::chrono::steady_clock::now();
 	}
 
 	inline void bench_stop()
 	{
 		end = std::chrono::steady_clock::now();
-		delta = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+		delta = std::chrono::duration_cast<Precision>(end - begin).count();
 	}
 
-	Bench() : delta(0) {}
+	BenchMark() : delta(0) {}
+};
+using Bench = BenchMark<>;
+
+template<class Precision = std::chrono::milliseconds>
+struct FPSBench
+{
+	std::chrono::steady_clock::time_point begin;
+	std::chrono::steady_clock::time_point end;
+	std::chrono::steady_clock::time_point mark_time;
+	float delta;
+
+	inline float get() const
+	{
+		return delta;
+	}
+
+	inline void bench_start()
+	{
+		begin = std::chrono::steady_clock::now();
+		mark_time = begin;
+	}
+
+	inline void mark()
+	{
+		delta = std::chrono::duration<float>(std::chrono::steady_clock::now() - mark_time).count();
+		mark_time = std::chrono::steady_clock::now();
+	}
+
+	inline void bench_stop()
+	{
+		end = std::chrono::steady_clock::now();
+		delta = std::chrono::duration<float>(end - begin).count();
+	}
+
+	FPSBench() : delta(0) {}
 };
 
 
